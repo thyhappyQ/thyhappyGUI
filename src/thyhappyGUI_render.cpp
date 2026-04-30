ac4bc37d13467ff15f395106c13337342e2be63f
thyhappy <busilpio@outlook.com>
2026-05-01 00:16:08 +0800

    add a function to create a d2d factory

--- src/thyhappyGUI_render.cpp
+++ src/thyhappyGUI_render.cpp
@@ -4,9 +4,25 @@
 
 #include "../include/thyhappyGUI_render.h"
 
-namespace thyhappy {
-    namespace init {
+#include <d2d1.h>
 
+#pragma comment(lib, "d2d1")
+
+namespace thyhappy {
+    ID2D1Factory* mf = nullptr; // Main factory
+    ID2D1HwndRenderTarget* mrt = nullptr; // Main render target
+
+    namespace init {
+        void createMainFactory(){
+            D2D1CreateFactory(D2D1_FACTORY_TYPE_MULTI_THREADED, &mf);
+            if (!mf) {
+                thyhappyError("Failed to create D2D factory");
+            }
+        }
+
+        void initialize() {
+
+        }
     }
 
     namespace render {

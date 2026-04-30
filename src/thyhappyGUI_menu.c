9bb27419713ab25379dc2811778816172187e85d
thyhappy <busilpio@outlook.com>
2026-04-30 19:58:05 +0800

    begin to develop the menu source

--- src/thyhappyGUI_menu.c
+++ src/thyhappyGUI_menu.c
@@ -3,3 +3,27 @@
 //
 
 #include "../include/thyhappyGUI_menu.h"
+
+void static tmRegisterWindow() {
+
+}
+
+void static tmCreateWindow() {
+
+}
+
+void static tmShowWindow() {
+
+}
+
+DLL inline void thyhappyMenuInitialize() {
+
+}
+
+DLL inline void thyhappyMenuRegister() {
+
+}
+
+DLL inline void thyhappyMenuCleanUp() {
+
+}

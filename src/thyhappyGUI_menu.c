7ec759e286aceb0cdb3d7da04fbf77d303581920
thyhappy <busilpio@outlook.com>
2026-04-30 20:00:45 +0800

    implement default menu window process

--- src/thyhappyGUI_menu.c
+++ src/thyhappyGUI_menu.c
@@ -4,7 +4,22 @@
 
 #include "../include/thyhappyGUI_menu.h"
 
+LRESULT CALLBACK wndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+            return 0;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+
+        default:return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+}
+
 void static tmRegisterWindow() {
+    WNDCLASS wc = {0};
 
 }
 

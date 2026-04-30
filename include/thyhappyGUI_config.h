feec244029f9bb2e568f3950e34cbbf495cf3dbe
thyhappy <busilpio@outlook.com>
2026-05-01 04:29:38 +0800

    fix some bugs

--- include/thyhappyGUI_config.h
+++ include/thyhappyGUI_config.h
@@ -5,4 +5,7 @@
 #ifndef THYHAPPYGUI_THYHAPPYGUI_CONFIG_H
 #define THYHAPPYGUI_THYHAPPYGUI_CONFIG_H
 
+// I use yyjson as JSON parser for now,I plan to use my own parser in future
+#include <yyjson.h>
+
 #endif //THYHAPPYGUI_THYHAPPYGUI_CONFIG_H

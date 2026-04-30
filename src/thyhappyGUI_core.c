41f1007c6a2be93f3560fe191eef114789f990e5
thyhappy <busilpio@outlook.com>
2026-04-30 17:58:41 +0800

    add a register window function in core source

--- src/thyhappyGUI_core.c
+++ src/thyhappyGUI_core.c
@@ -3,3 +3,31 @@
 //
 
 #include "../include/thyhappyGUI_core.h"
+
+LRESULT CALLBACK wndProc(HWND hwnd,UINT msg,WPARAM wParam,LPARAM lParam) {
+    switch (msg) {
+        default: return DefWindowProc(hwnd,msg,wParam,lParam);
+    }
+}
+
+void static tRegisterWindow() {
+    // Register a window
+    WNDCLASS wc = {0};
+
+    // Set window class properties
+    wc.lpszClassName = "ThyhappyGUI";
+    wc.lpfnWndProc = wndProc;
+    wc.hInstance = GetModuleHandle(nullptr);
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+    // Do with error
+    if (!RegisterClass(&wc)) {
+        return;
+    }
+}
+
+void thyhappyInitialize() {
+    /* Implement core initialize function */
+
+    tRegisterWindow();
+}

49f17cde76b6d380d98dea08c0232c08e3fb0aeb
thyhappy <busilpio@outlook.com>
2026-04-30 18:16:37 +0800

    implement user functions

--- src/thyhappyGUI_core.c
+++ src/thyhappyGUI_core.c
@@ -4,6 +4,38 @@
 
 #include "../include/thyhappyGUI_core.h"
 
+HWND Hwnd = nullptr;
+
+#define DEFAULT_WINDOW_TITLE "Default title"
+#define DEFAULT_WINDOW_WIDTH 800
+#define DEFAULT_WINDOW_HEIGHT 800
+#define DEFAULT_WINDOW_POS CW_USEDEFAULT
+
+int wWidth = DEFAULT_WINDOW_WIDTH;
+int wHeight = DEFAULT_WINDOW_HEIGHT;
+const char* wTitle = DEFAULT_WINDOW_TITLE;
+
+// Window positions
+int wX = DEFAULT_WINDOW_POS;
+int wY = DEFAULT_WINDOW_POS;
+
+DLL inline void thyhappySetTitle(const char* title) {
+    wTitle = title == nullptr ? "" : title;
+}
+
+DLL inline void thyhappySetWidth(const int width) {
+    wWidth = width;
+}
+
+DLL inline void thyhappySetHeight(const int height) {
+    wHeight = height;
+}
+
+DLL inline void thyhappySetPosition(const int x, const int y) {
+    wX = x < 0 ? 0 : x;
+    wY = y < 0 ? 0 : y;
+}
+
 LRESULT CALLBACK wndProc(HWND hwnd,UINT msg,WPARAM wParam,LPARAM lParam) {
     switch (msg) {
         default: return DefWindowProc(hwnd,msg,wParam,lParam);

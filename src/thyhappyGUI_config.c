6cc6d8466e82a7a3435a17d736367162ec51dfc9
thyhappy <busilpio@outlook.com>
2026-05-01 04:51:43 +0800

    add a function to read menu config

--- src/thyhappyGUI_config.c
+++ src/thyhappyGUI_config.c
@@ -14,6 +14,12 @@
 #define TITLE_KEY "title"
 #define BKCOLOR_KEY "bkcolor"
 
+#define M_WIDTH_KEY "width"
+#define M_BKCOLOR_KEY "bkcolor"
+#define M_MKBKCOLOR_KEY "mbbkcolor"
+#define M_EGDSTC_KEY "egdstc" // Edge distance
+#define M_BLDSTC_KEY "bldstc" // Block distance
+
 yyjson_doc* wDoc = NULL;
 yyjson_doc* mDoc = NULL;
 
@@ -39,6 +45,32 @@ void static tReadWindowConfig() {
         buffer[i] = (float)yyjson_get_num(yyjson_arr_get(bkArr,i));
     }
     thyhappySetBackgroundColor(buffer);
+}
+
+void static tReadMenuConfig() {
+    mDoc = yyjson_read_file(DEFAULT_MENU_CONFIG_PATH,0,NULL,NULL);
+    if (!mDoc) {
+        thyhappyError("Failed to read menu config file");
+        return;
+    }
+
+    yyjson_val* root = yyjson_doc_get_root(mDoc);
+
+    thyhappyMenuSetWidth(yyjson_get_int(yyjson_obj_get(root,M_WIDTH_KEY)));
+    thyhappyMenuSetEdgeDistance(yyjson_get_int(yyjson_obj_get(root,M_EGDSTC_KEY)));
+    thyhappyMenuSetBlockDistance(yyjson_get_int(yyjson_obj_get(root,M_BKCOLOR_KEY)));
+
+    // Read array
+    yyjson_val* mbclArr = yyjson_obj_get(root,M_BKCOLOR_KEY);
+    yyjson_val* mbbkArr = yyjson_obj_get(root,M_MKBKCOLOR_KEY);
+    float abuffer[4] = {0};
+    float bbuffer[4] = {0};
+    for (size_t i = 0; i < yyjson_arr_size(mbclArr); i++) {
+        abuffer[i] = (float)yyjson_get_num(yyjson_arr_get(mbclArr,i));
+        bbuffer[i] = (float)yyjson_get_num(yyjson_arr_get(mbbkArr,i));
+    }
+    thyhappyMenuSetBkColor(abuffer);
+    thyhappyMenuSetMbBkColor(bbuffer);
 }
 
 DLL void thyhappyConfigInitialize() {

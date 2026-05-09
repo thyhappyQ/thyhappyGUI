7757fa3143c259e376dbadc931a13d4a5f826fdd
thyhappy <busilpio@outlook.com>
2026-05-10 06:21:00 +0800

    Define a structure to save the app handle

---
+++ lib/src/thyhappy_gui.rs
@@ -0,0 +1,15 @@
+use winit;
+
+#[derive(Default)]
+struct thyhappyApp{
+    window :Option<winit::window::Window>
+}
+
+fn create_window(){
+
+}
+
+pub fn thyhappy_gui_initialize() {
+    // Initialize
+
+}

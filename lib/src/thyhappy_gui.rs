445797fd50a64bb98ab79a03f67ede8ba605345c
thyhappy <busilpio@outlook.com>
2026-05-10 09:44:02 +0800

    implement resumd and window_event functions for structure thyhappyApp

--- lib/src/thyhappy_gui.rs
+++ lib/src/thyhappy_gui.rs
@@ -1,15 +1,39 @@
 use winit;
+use winit::window::WindowAttributes;
 
 #[derive(Default)]
-struct thyhappyApp{
-    window :Option<winit::window::Window>
+struct thyhappyApp {
+    window: Option<winit::window::Window>,
 }
 
-fn create_window(){
+impl thyhappyApp {
+    fn resumd(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        // Check if the window is already created
+        if self.window.is_none() {
+            // Create window
+            self.window = Some(
+                event_loop.create_window(
+                    WindowAttributes::default()
+                        .with_title("hello"))
+                    .unwrap()
+            )
+        }
+    }
 
+    fn window_event(&mut self, event_loop: &winit::event_loop::ActiveEventLoop,
+                    id:winit::window::WindowId,event:&winit::event::WindowEvent) {
+        // Window messages loop
+        match event{
+            winit::event::WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+
+            // Default
+            _=>{}
+        }
+    }
 }
 
 pub fn thyhappy_gui_initialize() {
     // Initialize
-
 }
